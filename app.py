e4a90785ae51efe151c5fb55f86546002f5c4fd4
luhaszahu <21062@wc.school.nz>
2024-06-23 16:06:43 +1200

    Got my database working, and it is showing on the website. But it doesn't fit aesthetically, so I have to fix that.

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "LeMansRaceWinners.db"
+
+
+def create_connection(db_filename):
+    try:
+        connection = sqlite3.connect(db_filename)
+        return connection
+    except Error as e:
+        print(e)
+        return None
 
 
 @app.route('/')
@@ -10,12 +22,31 @@ def render_home_page():  # put application's code here
 
 @app.route('/webpages')
 def render_webpages_page():  # put application's code here
-    return render_template('webpages.html')
+
+    query = "SELECT Years, Drivers, Class, Team, Car, Tyre, Laps, Km, Mi, Series, Driver_nationality, Team_nationality, Average_speed_kmh, Average_speed_mph, Average_lap_time FROM LeMans_Winners WHERE Years >= 2000"
+    connection = create_connection(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute(query, )
+
+    data_list = cursor.fetchall()
+
+    return render_template('webpages.html', data=data_list)
 
 
 @app.route('/styles')
 def redner_styles_page():  # put application's code here
-    return render_template('styles.html')
+
+
+    query = "SELECT Years, Drivers, Class, Team, Car, Tyre, Laps, Km, Mi, Series, Driver_nationality, Team_nationality, Average_speed_kmh, Average_speed_mph, Average_lap_time FROM LeMans_Winners WHERE Years <= 1999"
+    connection = create_connection(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute(query, )
+
+    data_list = cursor.fetchall()
+
+    return render_template('webpages.html', data=data_list)
+
+
 
 if __name__ == '__main__':
     app.run()
