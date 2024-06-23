88bbaa22e47f24eac0d7d6c9ff6af6e24db4256d
luhaszahu <21062@wc.school.nz>
2024-06-23 17:14:23 +1200

    Got everything from the videos working now I just have to make changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 from sqlite3 import Error
 
@@ -20,32 +20,34 @@ def render_home_page():  # put application's code here
     return render_template('index.html')
 
 
-@app.route('/webpages')
-def render_webpages_page():  # put application's code here
+@app.route('/display/<table_type>')
+def render_display_page(table_type):  # put application's code here
 
-    query = "SELECT Years, Drivers, Class, Team, Car, Tyre, Laps, Km, Mi, Series, Driver_nationality, Team_nationality, Average_speed_kmh, Average_speed_mph, Average_lap_time FROM LeMans_Winners WHERE Years >= 2000"
+    query = "SELECT Years, Drivers, Class, Team, Car, Tyre, Laps, Km, Mi, Series, Driver_nationality, Team_nationality, Average_speed_kmh, Average_speed_mph, Average_lap_time FROM LeMans_Winners WHERE Team_nationality = ?"
     connection = create_connection(DATABASE)
     cursor = connection.cursor()
-    cursor.execute(query, )
+    cursor.execute(query, (table_type, ))
 
     data_list = cursor.fetchall()
 
-    return render_template('webpages.html', data=data_list)
+    return render_template('display.html', data=data_list, page_title=table_type)
 
 
-@app.route('/styles')
-def redner_styles_page():  # put application's code here
+@app.route('/search', methods=['GET', 'POST'])
+def render_search_page():  # put application's code here
 
+    look_up = request.form['Search']
+    title = "Search for: '" + look_up + "' "
+    look_up = "%" + look_up + "%"
 
-    query = "SELECT Years, Drivers, Class, Team, Car, Tyre, Laps, Km, Mi, Series, Driver_nationality, Team_nationality, Average_speed_kmh, Average_speed_mph, Average_lap_time FROM LeMans_Winners WHERE Years <= 1999"
+    query = "SELECT Years, Drivers, Class, Team, Car, Tyre, Laps, Km, Mi, Series, Driver_nationality, Team_nationality, Average_speed_kmh, Average_speed_mph, Average_lap_time FROM LeMans_Winners WHERE Years LIKE ? OR Drivers LIKE ? OR Class LIKE ? OR TEAM LIKE ? OR Car LIKE ? OR Tyre LIKE ? OR LAPS LIKE ? OR Km LIKE ? OR Mi LIKE ? OR Series LIKE ? OR Driver_nationality LIKE ? OR Team_nationality LIKE ? OR Average_speed_kmh LIKE ? OR Average_speed_mph LIKE ? OR Average_lap_time LIKE ?"
     connection = create_connection(DATABASE)
     cursor = connection.cursor()
-    cursor.execute(query, )
+    cursor.execute(query, (look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up, look_up))
 
     data_list = cursor.fetchall()
 
-    return render_template('webpages.html', data=data_list)
-
+    return render_template('display.html', data=data_list, page_title=title)
 
 
 if __name__ == '__main__':
